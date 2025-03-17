2bf71661032f4b473fb58763d0fe0d5ec5118e22
fiaZ3n <fiabot777@gmail.com>
2025-03-18 05:35:28 +0600

    Create Subarray Sums II (CSES).cpp
    https://cses.fi/problemset/task/1661/

---
+++ Subarray Sums II (CSES).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define int long long
+ 
+void fun() {
+    int n, target;
+    cin >> n >> target;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    map<int, int> mp;
+    mp[0]++;
+    int cnt = 0;
+    int butt = 0;  // prefix sum
+    for (int i = 1; i <= n; i++) {
+        butt += a[i];
+        cnt += mp[butt - target];
+        mp[butt] += 1;
+    }
+    cout << cnt << endl;
+}
+ 
+int32_t main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    optimize();
+ 
+    fun();
+ 
+    return 0;
+}
